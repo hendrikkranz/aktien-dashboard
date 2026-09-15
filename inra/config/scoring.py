c569459a21368983d85c2fafea316a82d6f13f04
Hendrik Kranz <hendrikkranz5@gmail.com>
2026-09-15 19:33:55 +0200

    Remove obsolete scoring code

--- inra/config/scoring.py
+++ inra/config/scoring.py
@@ -10,12 +10,3 @@ OPPORTUNITY_WEIGHTS = {
     "kgv_vs_branche": 3,
     "branchenbewertung_historisch": 3,
 }
-
-CHART_WEIGHTS = {
-    "momentum": 10,
-    "rsi": 8,
-    "hoch_52w": 7,
-    "trendkanal": 8,
-    "cm_macd": 7,
-    "unterstuetzung": 5,
-}
